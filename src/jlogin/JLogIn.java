f23e760d57ea554a514cfb92414e3107dadbfcbe
Travis Mitchum <travism2006@gmail.com>
2018-01-07 10:29:04 -0500

    Small changes
    just some small changes

--- src/jlogin/JLogIn.java
+++ src/jlogin/JLogIn.java
@@ -1,5 +1,7 @@
 package jlogin;
 
+import java.awt.Container;
+
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -17,7 +19,25 @@ import javax.swing.JOptionPane;
  * -does not use character hiding on the password input text field<p>
  * -assumes string input for both login type of input, including the password<p>
  * 
-
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  * @version 1.0
  * @author tmitchu2
  * */
@@ -29,60 +49,92 @@ public class JLogIn extends JDialog
 	//to avoid 'magic numbers'
 	private static final int COLWIDTH = 15;
 	private static final int WIDTH = 100;
-	private static final int HEIGHT = 25;
+	private static final int HEIGHT = 50;
 	
+	/***/
 	public static final int SHOW_MASK = 0;
 	public static final int HIDE_MASK = -1;
+	public static final int DEFAULT_MASK = 0;
 	
 	/**
 	 * default, auto-generated
 	 */
 	private static final long serialVersionUID = 1L;
 
-	/**Default contructor only used for initializing small frame window size*/
+	/**Defatil contructor only used for initializing small frame window size*/
 	public JLogIn()
-	{
-		setSize(WIDTH, HEIGHT);
-	}
+	{setSize(WIDTH, HEIGHT);}
 	
+	/*Make the necessary buttons*/
 	JButton yesBtn = new JButton("Yes");
 	JButton noBtn = new JButton("No");
+	JButton cancelBtn = new JButton("Cancel");
+	JButton okBtn = new JButton("OK");
+	
+	public static final int a=-1;
+	
 	
 	/**
 	 * 
-	 * @return session acceptance or denial based on their credentials
+	 * @return session acceptance or denail based on their credentials
 	 * */
 	public static int showLogIn(int maskDeterminant)
 	{
-		JPanel cont = new JPanel();
-		cont.setLayout(new BoxLayout(cont, BoxLayout.X_AXIS));
+		JPanel temp = new JPanel();
+		temp.setLayout(new BoxLayout(temp, BoxLayout.X_AXIS));
 		
 		JLabel idLbl = new JLabel("Login ID:");
 		JTextField idField = new JTextField(COLWIDTH);
-		cont.add(idLbl);
-		cont.add(idField);
+		temp.add(idLbl);temp.add(idField);
 		
 		JLabel passLbl = new JLabel("Password");
-		
-		cont.add(passLbl);
+		temp.add(passLbl);
 		
 		if(maskDeterminant == SHOW_MASK)
-		{
-			JTextField passField = new JPasswordField(COLWIDTH);
-			cont.add(passField);
-		}
+		{JTextField passField = new JPasswordField(COLWIDTH);temp.add(passField);}
 		else if(maskDeterminant == HIDE_MASK)
-		{
-			JTextField passField = new JTextField(COLWIDTH);
-			cont.add(passField);
-		}
+		{JTextField passField = new JTextField(COLWIDTH);temp.add(passField);}
 		
 		//now add buttons
+		JPanel cont = new JPanel();
+		cont.setLayout(new BoxLayout(cont, BoxLayout.Y_AXIS));
+		cont.add(temp);
+		
+		return showLogIn(DEFAULT_MASK, cont);
+	}
 	
-		return JOptionPane.showConfirmDialog(null, cont, "", JOptionPane.YES_NO_OPTION);
+	//, "", JOptionPane.YES_NO_OPTION
+	
+	/***/
+	public static int showLogIn(int maskDeterminant, Container aCont)
+	{
+		
+		
+		return showLogIn(DEFAULT_MASK,aCont, "");
+	}
+
+	public static int showLogIn(int maskDeterminant, Container aCont, String title)
+	{
+		
+		
+		return 0;
 	}
 	
 	
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
